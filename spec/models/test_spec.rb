5f7e0c9ab4318f93ec2d3142146e558bfacd267c
evandro.castro <evandro.castro.prestador@solides.com.br>
2025-04-16 14:23:15 -0300

    feat: base project code, set up docker_compose, dockerfile, rubocop, rspec, ci/cd

---
+++ spec/models/test_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Test do
+  describe 'validations' do
+  end
+
+  describe 'test create' do
+    before { .save }
+
+    it { is_expected.to be_persisted }
+  end
+end
