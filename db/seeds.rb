22df5131cf0222182a5941098c35a6a9ab97c69b
evandro.castro <evandro.castro.prestador@solides.com.br>
2025-04-16 16:51:22 -0300

    feat: comments in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# create_table :service_orders do |t|
+#   t.string :code, null: false
+#   t.string :farm_name, null: false
+#   t.string :status, default: :pending
+#   t.float :farm_area, null: false
+#   t.float :coverage_area, default: 0
+#   t.date :date, null: false
+
+#   t.references :user, null: false, foreign_key: true
+#    add_index :service_orders, :email, unique: true
+
+#   t.timestamps
+# end
